e52c16a762a55fd18db8c5da2344a6e02303527b
Brendan Irvine-Broque <brendanib@gmail.com>
2025-03-10 22:29:36 -0700

    fixup

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,10 @@ export default {
 		const url = new URL(request.url);
 		let sessionId = url.searchParams.get('sessionId');
 
-		if (!sessionId) {
-			sessionId = env.MCP_AGENTS.newUniqueId();
-		}
+		const mcpServerAgent = env.MCP_AGENTS.get(
+			sessionId ? env.MCP_AGENTS.idFromString(sessionId) : env.MCP_AGENTS.newUniqueId()
+		);
 
-		const mcpServerAgent = env.MCP_AGENTS.get(sessionId);
 		return mcpServerAgent.fetch(request);
 	},
 }; 
